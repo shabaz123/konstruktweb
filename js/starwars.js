c74f94a588542e0dc17d321b948afe5f66afc70f
Mike Back <mikeback75@hotmail.com>
2021-03-29 11:23:24 +1100

    add Javascript event listeners to Star Wars page

---
+++ js/starwars.js
@@ -0,0 +1,29 @@
+const swPlanet = document.querySelector("#planets");
+swPlanet.addEventListener("click", () =>
+   alert("You clicked the PLANETS category!")
+);
+
+const swSpaceships = document.querySelector("#spaceships");
+swSpaceships.addEventListener("click", () =>
+   alert("You clicked the SPACESHIPS category!")
+);
+
+const swVehicles = document.querySelector("#vehicles");
+swVehicles.addEventListener("click", () =>
+   alert("You clicked the VEHICLES category!")
+);
+
+const swPeople = document.querySelector("#people");
+swPeople.addEventListener("click", () =>
+   alert("You clicked the PEOPLE category!")
+);
+
+const swFilms = document.querySelector("#films");
+swFilms.addEventListener("click", () =>
+   alert("You clicked the FILMS category!")
+);
+
+const swSpecies = document.querySelector("#species");
+swSpecies.addEventListener("click", () =>
+   alert("You clicked the SPECIES category!")
+);
